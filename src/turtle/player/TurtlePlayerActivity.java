e8a3ac6985f7581a015c0142e66cb1482b8df285
Edd Turtle <eddturtle@gmail.com>
2012-06-15 12:49:58 +0100

    Initial Blank Android Project

---
+++ src/turtle/player/TurtlePlayerActivity.java
@@ -0,0 +1,13 @@
+package turtle.player;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TurtlePlayerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
