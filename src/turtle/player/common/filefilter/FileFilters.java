6add5dc26b0169f5cbcb5139337af7fa884c249d
Simon Honegger <simon.honegger@gmail.com>
2012-10-07 01:30:44 +0200

    using regex for file filter

--- src/turtle/player/common/filefilter/FileFilters.java
+++ src/turtle/player/common/filefilter/FileFilters.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 public interface FileFilters
 {
-    final static FilenameFilter PLAYABLE_FILES_FILTER = new ExtensionsFilter(".mp3", ".ogg");
+    final static FilenameFilter PLAYABLE_FILES_FILTER = new NameFilter("*.mp3", "*.ogg");
 
     final static List<? extends FilenameFilter> folderArtFilters = Arrays.asList(
             new NameFilter("folder.jpg"),
-            new ExtensionsFilter(".jpg", ".gif")
+            new NameFilter("*front*.jpg", "*front*.gif"),
+            new NameFilter("*cover*.jpg", "*cover*.jpeg", "*cover*.gif"),
+            new NameFilter("*.jpg", "*.jpeg", "*.gif")
     );
 }
--- src/turtle/player/common/filefilter/NameFilter.java
+++ src/turtle/player/common/filefilter/NameFilter.java
@@ -12,13 +12,13 @@ public class NameFilter extends  AccessableFileFilter
     }
 
     @Override
-    public boolean accept(File dir, String filename)
+    public boolean accept(File dir, String pattern)
     {
-        if(super.accept(dir, filename))
+        if(super.accept(dir, pattern))
         {
             for(String name : names)
             {
-                if(name.equals(filename))
+                if(name.toLowerCase().matches(pattern))
                 {
                     return true;
                 }
