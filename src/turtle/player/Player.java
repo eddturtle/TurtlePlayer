ff6e3356486d64123e2ecc9555488437941d3b31
Edd Turtle <eddturtle@gmail.com>
2013-02-18 16:40:27 +0000

    Fix Millisecond Crash Issue

--- src/turtle/player/Player.java
+++ src/turtle/player/Player.java
@@ -594,7 +594,14 @@ public class Player extends ListActivity
 		else
 		{
 			tp.player.change(restoredTrack);
-			tp.player.goToMillis(tp.playlist.preferences.get(Keys.EXIT_PLAY_TIME));
+			if (tp.playlist.preferences.get(Keys.EXIT_PLAY_TIME) != null)
+			{
+				tp.player.goToMillis(tp.playlist.preferences.get(Keys.EXIT_PLAY_TIME));
+			}
+			else
+			{
+				tp.player.goToMillis(0);
+			}
 		}
 
 	}
