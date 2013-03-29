e51b96d21742f4dd9df1c80f29f491eada92923f
Simon Honegger <simon.honegger@gmail.com>
2013-03-29 23:10:18 +0100

    update filechooser after remove filter

--- src/turtle/player/view/FileChooser.java
+++ src/turtle/player/view/FileChooser.java
@@ -88,6 +88,7 @@ public abstract class FileChooser implements TurtleDatabase.DbObserver
 			{
 				filters.remove(filter);
 				filterListAdapter.remove(filter);
+				update();
 			}
 
 			@Override
