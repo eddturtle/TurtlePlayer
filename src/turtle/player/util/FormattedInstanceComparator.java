91e79626c64eceb2d34d8b53f89c768811cbc8ca
Simon Honegger <simon.honegger@gmail.com>
2013-06-01 17:09:01 +0200

    fix file chooser order while reading file system

--- src/turtle/player/util/FormattedInstanceComparator.java
+++ src/turtle/player/util/FormattedInstanceComparator.java
@@ -18,15 +18,20 @@
 
 package turtle.player.util;
 
-import turtle.player.model.Instance;
+import turtle.player.model.*;
 import turtle.player.presentation.InstanceFormatter;
 
 import java.text.Collator;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 
 public class FormattedInstanceComparator implements Comparator<Instance>
 {
 	private final InstanceFormatter listInstanceFormatter;
+
+
 
 	public FormattedInstanceComparator(InstanceFormatter listInstanceFormatter)
 	{
@@ -36,8 +41,46 @@ public class FormattedInstanceComparator implements Comparator<Instance>
 	public int compare(Instance lhs,
 							 Instance rhs)
 	{
-		return Collator.getInstance().compare(lhs.accept(listInstanceFormatter), rhs.accept(listInstanceFormatter));
+		int lhsWeight = getOrderWeight(lhs);
+		int rhsWeight = getOrderWeight(rhs);
+		int weightDiff = new Integer(lhsWeight).compareTo(rhsWeight);
+		return weightDiff != 0 ? weightDiff :
+				  Collator.getInstance().compare(lhs.accept(listInstanceFormatter), rhs.accept(listInstanceFormatter));
 	}
 
+	private int getOrderWeight(Instance instance)
+	{
+		return instance.accept(new InstanceVisitor<Integer>()
+		{
+			public Integer visit(Track track)
+			{
+				return 5;
+			}
 
+			public Integer visit(SongDigest track)
+			{
+				return 5;
+			}
+
+			public Integer visit(Album album)
+			{
+				return 4;
+			}
+
+			public Integer visit(GenreDigest genre)
+			{
+				return 2;
+			}
+
+			public Integer visit(ArtistDigest artist)
+			{
+				return 3;
+			}
+
+			public Integer visit(FSobject FSobject)
+			{
+				return 1;
+			}
+		});
+	}
 }
